1a100bcc6dd6f6e6277c902ed8c044a7ec71c523
Vipin Sharma <vsharma@gainsight.com>
2017-09-08 20:35:40 +0530

    Added quick sort and postfix expression evaluation

--- src/main/java/com/vips/algorithm/sort/QuickSort.java
+++ src/main/java/com/vips/algorithm/sort/QuickSort.java
@@ -4,20 +4,42 @@ import com.vips.algorithm.Algorithms;
 
 /**
  * @author Vipin Sharma
- * @category <blockquote></blockquote>
+ * @category <blockquote>concepts</blockquote>
  * @DATE 7/9/17
  **/
 public class QuickSort implements Algorithms {
 
 
-    public void sort(int[] array){
+    public void sort(int[] array, int low, int high){
+        if(low >= high) return;
+        int pivot = pivot(array,low,high);
+        sort(array,low,pivot-1);
+        sort(array,pivot+1,high);
+    }
 
+    private int pivot(int[] arr, int low, int high){
+        int pivotIndex = (high + low)/2;
+        swap(arr,high,pivotIndex);
+
+        int i = low;
+        for(int j = low; j < high ; j++){
+            if(arr[j] <= arr[high]){
+                swap(arr,i,j);
+                i++;
+            }
+        }
+        swap(arr,i,high);
+        return i;
     }
 
     public static void main(String[] args) {
         QuickSort sort = new QuickSort();
 
         int arr [] = {1,21,3,32,42,3,12};
-        sort.sort(arr);
+        System.out.print("\n Before sorting : ");
+        sort.printArray(arr);
+        sort.sort(arr,0,arr.length-1);
+        System.out.print("\n After sorting :  ");
+        sort.printArray(arr);
     }
 }
